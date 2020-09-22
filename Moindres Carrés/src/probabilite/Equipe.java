d5a5798efff80d380d9b8f8998b0e40f27d74c44
Maël Le Folgoc Pontis <37334769+Mael-Le-Folgoc-Pontis@users.noreply.github.com>
2020-09-22 11:12:56 +0200

    v1 mais la vrai

---
+++ Moindres Carrés/src/probabilite/Equipe.java
@@ -0,0 +1,11 @@
+package probabilite;
+
+import java.util.ArrayList;
+
+public class Equipe {
+	
+	private String nomEquipe;
+	
+	private ArrayList<Match> match;
+
+}
---
+++ Moindres Carrés/src/probabilite/Match.java
@@ -0,0 +1,37 @@
+package probabilite;
+
+public class Match {
+	
+	private String equipe1;
+	
+	private String equipe2;
+	
+	private int score1;
+	
+	private int score2;
+
+	public Match(String equipe1, String equipe2, int score1, int score2) {
+		super();
+		this.equipe1 = equipe1;
+		this.equipe2 = equipe2;
+		this.score1 = score1;
+		this.score2 = score2;
+	}
+
+	public String getEquipe1() {
+		return equipe1;
+	}
+
+	public String getEquipe2() {
+		return equipe2;
+	}
+
+	public int getScore1() {
+		return score1;
+	}
+
+	public int getScore2() {
+		return score2;
+	}
+
+}
